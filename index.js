3feed4a7f83d00f4285bf8c6f470362a38867382
Matthew Centeri <mattcenteri@gmail.com>
2021-03-16 21:24:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,7 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase()===string){
     return "I can't hear you!";
     }if(string.toUpperCase()===string){
+      return "YES INDEED!";
+    }
   }
 }
