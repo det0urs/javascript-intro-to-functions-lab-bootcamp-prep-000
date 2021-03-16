ef10bbc71e1000fd16f3a5aa11c541115aaf0fb8
Matthew Centeri <mattcenteri@gmail.com>
2021-03-16 21:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ console.log (lowercase);
 }
 function sayHiToGrandma(string) {
   if(string.toLowerCase()===string){
-    return "I can't hear you!"
+    return "I can't hear you!";
+    if(string.toUpperCase()===string){
   }
 }
